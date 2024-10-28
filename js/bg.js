c27992809fda8ee77151566c959bbae2bcec7239
SUBIN-LSB <144414519+SUBIN-LSB@users.noreply.github.com>
2024-10-28 23:03:10 +0900

    Update bg.js add bg_container_box

--- js/bg.js
+++ js/bg.js
@@ -1,17 +1,22 @@
-
 // sec3의 bg_svg 애니메이션 부분 스크립트
 document.addEventListener("DOMContentLoaded", function () {
-    const target = document.querySelector('.bg_svg');
+    const BgSvgTarget = document.querySelector('.bg_svg');
+    const SvgContainerTarget = document.querySelector('.bg_container_box');
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                target.classList.add("show");
+                if (entry.target === BgSvgTarget) {
+                    BgSvgTarget.classList.add("show");
+                } else if (entry.target === SvgContainerTarget) {
+                    SvgContainerTarget.classList.add("up");
+                }
                 observer.unobserve(entry.target);
             }
         });
     }, { threshold: 0.3 });
 
-    observer.observe(target);
+    observer.observe(BgSvgTarget);
+    observer.observe(SvgContainerTarget);
 
 });
