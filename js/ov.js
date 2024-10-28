44c7b8e41de00d955109061306831fb0418101c4
SUBIN-LSB <144414519+SUBIN-LSB@users.noreply.github.com>
2024-10-28 20:58:33 +0900

    Update ov.js add bg_svg

--- js/ov.js
+++ js/ov.js
@@ -1,1 +1,17 @@
 
+// sec3의 bg_svg 애니메이션 부분 스크립트
+document.addEventListener("DOMContentLoaded", function () {
+    const target = document.querySelector('.bg_svg');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                target.classList.add("show");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.3 });
+
+    observer.observe(target);
+
+});
