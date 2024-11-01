99eec6cdfa844f3d107446d1349388b72f07c204
liangexe <sundrymy@naver.com>
2024-11-02 00:47:49 +0900

    ob js up

--- js/ob.js
+++ js/ob.js
@@ -13,3 +13,19 @@ document.querySelector('.ob-scroll').addEventListener('click', function () {
 
     document.querySelector('.ob-svg-list').style.transform = `translateX(${newTranslateX}px)`;
 });
+
+document.querySelector('.bl-scroll').addEventListener('click', function () {
+
+    const scrollAmount = 245 + 40;
+    const maxTranslateX = -scrollAmount * 6;
+
+    let currentTranslateX = parseFloat(getComputedStyle(document.querySelector('.bl-svg-list')).transform.split(',')[4]) || 0;
+
+    let newTranslateX = currentTranslateX - scrollAmount;
+
+    if (newTranslateX < maxTranslateX) {
+        newTranslateX = 0;
+    }
+
+    document.querySelector('.bl-svg-list').style.transform = `translateX(${newTranslateX}px)`;
+});
